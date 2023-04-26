020f29bef6dc2bdd14cbe42ee75dd616993dc687
Asad0002 <130868233+Asad0002@users.noreply.github.com>
2023-04-26 21:48:43 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+// document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamA').addEventListener('click', ()=> {
+    document.querySelector('.sidebar').classList.toggle('.sidebarGo');
+})
+document.querySelector('.hamB').addEventListener('click', ()=> {
+    document.querySelector('.sidebar').classList.toggle('.sidebarGo');
+})
